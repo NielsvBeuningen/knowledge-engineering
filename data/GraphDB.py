57b82a45e9835a371ab4fbea737ee61bc65ff1ef
Niels <n.p.g.t.v.beuningen@student.tue.nl>
2024-06-03 16:52:27 +0200

    updated for kg upgrading

--- data/GraphDB.py
+++ data/GraphDB.py
@@ -11,16 +11,16 @@ class GraphDB:
         self.driver.close()
 
     ### COMPANY
-    def create_sa2(self, sa2_5dig, sa2_name, area):
+    def create_sa2(self, sa2_5dig, sa2_name, area, population, pop_percentage, pop_density):
         with self.driver.session(database=self.database) as session:
-            return session.write_transaction(self._create_sa2, sa2_5dig, sa2_name, area)
+            return session.write_transaction(self._create_sa2, sa2_5dig, sa2_name, area, population, pop_percentage, pop_density)
         
     @staticmethod
-    def _create_sa2(tx, sa2_5dig, sa2_name, area):
+    def _create_sa2(tx, sa2_5dig, sa2_name, area, population, pop_percentage, pop_density):
         query = (
-            "CREATE (s:SA2 {id: $sa2_5dig, sa2_name: $sa2_name, area: $area}) "
+            "CREATE (s:SA2 {id: $sa2_5dig, sa2_name: $sa2_name, area: $area, population: $population, pop_percentage: $pop_percentage, pop_density: $pop_density}) "
         )
-        tx.run(query, sa2_5dig=sa2_5dig, sa2_name=sa2_name, area=area)
+        tx.run(query, sa2_5dig=sa2_5dig, sa2_name=sa2_name, area=area, population=population, pop_percentage=pop_percentage, pop_density=pop_density)
 
     def create_hospital(
         self, hospital_id, hospital_name, 
@@ -54,14 +54,14 @@ class GraphDB:
                description=description, sector=sector, beds=beds, latitude=latitude, 
                longitude=longitude)
 
-    def add_relation_sa2_hospital(self, hospital_id, sa2_5dig, distance_time):
+    def add_relation_sa2_hospital(self, hospital_id, sa2_5dig, distance_time, accessible, further_than_2h):
         with self.driver.session(database=self.database) as session:
-            session.write_transaction(self._add_relation_sa2_hospital, hospital_id, sa2_5dig, distance_time)
+            session.write_transaction(self._add_relation_sa2_hospital, hospital_id, sa2_5dig, distance_time, accessible, further_than_2h)
 
     @staticmethod
-    def _add_relation_sa2_hospital(tx, hospital_id, sa2_5dig, distance_time):
+    def _add_relation_sa2_hospital(tx, hospital_id, sa2_5dig, distance_time, accessible, further_than_2h):
         query = (
-            "MATCH (s:SA2 {id: $sa2_5dig}) (h:Hospital {id: $hospital_id}) "
-            "CREATE (h)-[:REACHABLE_VIA {distance_time: $distance_time}]->(s) "
+            "MATCH (h:Hospital {id: $hospital_id}), (s:SA2 {id: $sa2_5dig}) "
+            "CREATE (h)-[:REACHABLE_VIA {distance_time: $distance_time, accessible: $accessible, further_than_2h: $further_than_2h}]->(s) "
         )
-        tx.run(query, hospital_id=hospital_id, sa2_5dig=sa2_5dig, distance_time=distance_time)
+        tx.run(query, hospital_id=hospital_id, sa2_5dig=sa2_5dig, distance_time=distance_time, accessible=accessible, further_than_2h=further_than_2h)
